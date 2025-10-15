92cf00c439e3c18c459c04faa265bc019e856093
Vladymyr-89-Nester <paladinsvetoi@gmail.com>
2025-10-15 11:48:46 +0300

    modal-update

--- src/modal.js
+++ src/modal.js
@@ -3,16 +3,26 @@ const closeBtnEl = document.querySelector(".modal-close-btn")
 const modalMenuEl = document.querySelector(".backdrop")
 const closeModalBtnEl = document.querySelector(".modal-button")
 const formRegiEl = document.querySelector(".register-form")
+const regInputEl = document.querySelector(".register-input")
 const bodyEl = document.body;
 
 closeModalBtnEl.addEventListener('click', toggleModal)
-formBtnEl.addEventListener('click', toggleModal) 
 closeBtnEl.addEventListener('click', toggleModal)
 
 formRegiEl.addEventListener('submit', function(event) {
-  event.preventDefault();  
+  event.preventDefault();
+  const inputValue = regInputEl.value.trim();
+
+  if (inputValue === '') {
+    
+    alert('The field cannot be empty!');
+  } else {
+    
+    toggleModal();
+  }
 });
 
+  
 function toggleModal() {
     modalMenuEl.classList.toggle("is-open")
 
