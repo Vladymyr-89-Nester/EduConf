bdb77e5136dbb436bcc1bc3eedd7f11c8c7bd9b3
Vladymyr-89-Nester <paladinsvetoi@gmail.com>
2025-10-15 13:53:55 +0300

    mob-menu-btn-add

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,12 @@
 const burgerEl = document.querySelector(".burger-menu-header")
 const closeBtnEl = document.querySelector(".menu-close-btn")
 const mobMenuEl = document.querySelector(".burger-menu")
+const mobLink = document.querySelector(".menu-register-btn")
 const bodyEl = document.body;
 
 burgerEl.addEventListener('click', toggleModal) 
 closeBtnEl.addEventListener('click', toggleModal)
+mobLink.addEventListener('click', toggleModal)
 
 function toggleModal() {
     mobMenuEl.classList.toggle("is-open")
